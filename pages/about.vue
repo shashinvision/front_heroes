8e01c598264dcfdf39d572c3de352d687b4c614b
Felipe Mancilla <felipei.mancilla@gmail.com>
2022-02-01 17:59:41 -0300

    ADD init commit

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FrontHeroesAbout",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped></style>
