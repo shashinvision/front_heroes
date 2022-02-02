88e7ee6feab5bd74479b1db76927df7c16d8e950
Felipe Mancilla <felipei.mancilla@gmail.com>
2022-02-02 09:55:58 -0300

    ADD - contador

---
+++ components/contador.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "FrontHeroesContador",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="css" scoped></style>
