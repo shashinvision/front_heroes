61baa16ef4128c7bb68d0ae57bca3bfa75525861
Felipe Mancilla <felipei.mancilla@gmail.com>
2022-02-02 10:34:03 -0300

    ADD csss contador

--- components/contador.vue
+++ components/contador.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="contador-container"></div>
+  <div class="contador-container">
+    <div class="content">
+      <div class="label-container">
+        <label>Lorem ipsum dolor sit amet.</label>
+      </div>
+      <div class="button-container centrar">
+        <button>-</button>
+        <label class="label-contador"> 123</label>
+        <button>+</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -20,8 +31,29 @@ export default {
 .contador-container {
   margin-top: 2%;
   width: 100%;
-  height: 50px;
+  height: auto;
   border-radius: 3px;
   background-color: #a5d6a7 !important;
+}
+.content {
+  display: flex;
+  margin-left: 10px;
+  width: 100%;
+}
+.label-container {
+  align-items: center;
+}
+.button-container {
+  margin-right: 10% !important;
+  display: flex;
+  align-items: center;
+}
+.label-contador {
+  padding: 0 10px 0 10px;
+}
+button {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
 }
 </style>
