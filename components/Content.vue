58f8c0fefd88eb2bea97bf98442bed60a35825c8
Felipe Mancilla <felipei.mancilla@gmail.com>
2022-02-04 09:23:24 -0300

    ADD Sort

--- components/Content.vue
+++ components/Content.vue
@@ -23,13 +23,50 @@ export default {
   computed: {
     ...mapGetters(["getContadores"]),
     ordenContadores() {
-      let resultado = { ...this.getContadores };
+      let arreglo = [];
+      for (let i = 0; i < this.getContadores.length; i++) {
+        arreglo.push(this.getContadores[i]);
+      }
 
       if (this.orden == "asc") {
-        console.log(resultado);
+        // console.log(this.getContadores);
+        // console.log(arreglo);
+
+        arreglo.sort((a, b) => {
+          if (a.contador < b.contador) {
+            return -1;
+          }
+          if (a.contador > b.contador) {
+            return 1;
+          }
+          if (a.nombre.toLowerCase() < b.nombre.toLowerCase()) {
+            return -1;
+          }
+          if (a.nombre.toLowerCase() > b.nombre.toLowerCase()) {
+            return 1;
+          }
+          return 0;
+        });
+        console.log(arreglo);
       } else if (this.orden == "desc") {
+        arreglo.sort((a, b) => {
+          if (b.contador < a.contador) {
+            return -1;
+          }
+          if (b.contador > a.contador) {
+            return 1;
+          }
+          if (b.nombre.toLowerCase() < a.nombre.toLowerCase()) {
+            return -1;
+          }
+          if (b.nombre.toLowerCase() > a.nombre.toLowerCase()) {
+            return 1;
+          }
+          return 0;
+        });
+        console.log(arreglo);
       }
-      return resultado;
+      return arreglo;
     },
   },
   data() {
