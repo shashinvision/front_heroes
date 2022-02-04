3e93195f7adc6c01db5e470f9cce167d567c937d
Felipe Mancilla <felipei.mancilla@gmail.com>
2022-02-04 12:09:54 -0300

    hotfix sort contador

--- components/Content.vue
+++ components/Content.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="contenedor" class="content centrar">
     <select name="" id="" v-model="orden">
-      <option value="">-- Orden --</option>
+      <option value="">-- Orden por contrador --</option>
       <option value="asc">Ascendente</option>
       <option value="desc">Descendente</option>
     </select>
-    <div v-for="(dataContador, index) in getContadores" :key="index">
+    <div v-for="(dataContador, index) in ordenContadores" :key="index">
       <contador :dataContador="{ dataContador, index }" />
     </div>
   </div>
@@ -23,15 +23,10 @@ export default {
   computed: {
     ...mapGetters(["getContadores"]),
     ordenContadores() {
-      for (let i = 0; i < this.clonObjt.length; i++) {
-        this.contadores.push(this.clonObjt[i]);
-      }
+      let clonObj = JSON.parse(JSON.stringify(this.getContadores));
 
       if (this.orden == "asc") {
-        // console.log(this.getContadores);
-        // console.log(contadores);
-
-        this.contadores.sort((a, b) => {
+        clonObj.sort((a, b) => {
           if (a.contador < b.contador) {
             return -1;
           }
@@ -46,9 +41,9 @@ export default {
           }
           return 0;
         });
-        console.log(this.contadores);
+        // console.log(this.contadores);
       } else if (this.orden == "desc") {
-        this.contadores.sort((a, b) => {
+        clonObj.sort((a, b) => {
           if (b.contador < a.contador) {
             return -1;
           }
@@ -63,17 +58,16 @@ export default {
           }
           return 0;
         });
-        console.log(this.contadores);
       }
-      // this.updateContador(this.contadores);
-      return this.contadores;
+      console.log(this.clonObj);
+
+      // this.updateContador(clonObj);
+      return clonObj;
     },
   },
   data() {
     return {
       orden: "",
-      contadores: [],
-      clonObjt: { ...this.getContadores },
     };
   },
 
@@ -83,9 +77,9 @@ export default {
     ...mapActions({
       actualizarContador: "actualizarContadorAction",
     }),
-    // updateContador(payload) {
-    //   this.actualizarContador(payload);
-    // },
+    updateContador(payload) {
+      this.actualizarContador(payload);
+    },
   },
 };
 </script>
